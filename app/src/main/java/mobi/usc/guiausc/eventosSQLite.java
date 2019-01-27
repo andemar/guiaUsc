016f43d7d8de92325fc6a4e329791bae1b39147a
andemar <andemartinez98@hotmail.com>
2019-01-27 14:22:07 -0500

    Boton 2 y 3 listos. (Falta la verificacion de cambios y almacenar imagen en el SQLite).

---
+++ app/src/main/java/mobi/usc/guiausc/eventosSQLite.java
@@ -0,0 +1,32 @@
+package mobi.usc.guiausc;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+
+public class eventosSQLite extends SQLiteOpenHelper {
+
+    public eventosSQLite(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        /**
+         * idEvento = La llave primaria y el numero del dato.
+         * Nombre = Nombre del evento.
+         * Fecha = La fecha del evento.
+         * Ponentes = Los ponentes del evento.
+         * Descripcion = La descripcion del evento.
+         * Ubicacion = La ubicacion del evento.
+         */
+        //db.execSQL("create table eventosInscritos(idEvento String primary key,  Nombre String, Fecha String, Ponentes String, Descripcion String, Ubicacion String)");
+        db.execSQL("create table eventosInscritos(idEvento String primary key,  Nombre String, Fecha String, Hora String, Ubicacion String, Ponente String, Descripcion String)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
